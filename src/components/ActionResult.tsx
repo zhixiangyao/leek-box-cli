ae5c37aa088e312e1ec2793cc8c5ecbf39d46efb
zhixiangyao <blueskyzx@icloud.com>
2026-08-27 08:09:00 +0000

    refactor: 简化组件实现

--- src/components/ActionResult.tsx
+++ src/components/ActionResult.tsx
@@ -1,5 +1,8 @@
-import BackToStockList from './BackToStockList.tsx'
+import { Newline } from 'ink'
+
+import { useRouterStore } from '../stores/useRouterStore.ts'
 import Message, { type MessageTone } from './Message.tsx'
+import TextInput from './TextInput.tsx'
 
 type Props = {
   tone: MessageTone
@@ -7,10 +10,13 @@ type Props = {
 }
 
 export default function ActionResult({ tone, msg }: Props) {
+  const goTo = useRouterStore((state) => state.goTo)
+
   return (
     <>
       <Message tone={tone} msg={msg} />
-      <BackToStockList />
+      <Newline />
+      <TextInput prompt="按 Enter 返回自选股票看板..." onSubmit={() => goTo('stock-list')} />
     </>
   )
 }
